dc3392168c84eb3a79967d11219c0326ff5b4a5c
vaibhavxcitech <119841873+vaibhavxcitech@users.noreply.github.com>
2023-09-25 18:13:06 +0530

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+
+  const handleLogin = async () => {
+    console.log("login api called");
+    try {
+      const response = await fetch('https://stagev2a.rechargkit.biz/member/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+        const data = await response.json();
+        if(data.token){
+        setToken(data.token);
+        scheduleTokenRefresh();
+      } else {
+        window.alert("Something went wrong, please try again.");
+      }
+    } catch (error) {
+      window.alert("Something went wrong, please try again.");
+    }
+  };
+
+  const refreshToken = async () => {
+    console.log("refresh token called");
+    try {
+      const response = await fetch('https://stagev2a.rechargkit.biz/member/token', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+        const data = await response.json();
+        if (data.token) {
+        setToken(data.token);
+        scheduleTokenRefresh();
+      } else {
+      }
+    } catch (error) {
+    }
+  };
+
+  const scheduleTokenRefresh = () => {
+    const tokenRefreshInterval = setInterval(() => {
+      refreshToken();
+    }, 10000);
+    return () => clearInterval(tokenRefreshInterval);
+  };
+
+  useEffect(() => {
+    if (token) {
+      scheduleTokenRefresh();
+    }
+  }, [token]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
